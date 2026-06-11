6aa8ad47e2ec8f8cbb188473b29d6442a8f3161f
Picolino66 <isaiasbd@hotmail.com>
2026-06-10 23:29:10 -0300

    fix: buckets.module.ts agora importa PrismaModule e ApiKeysModule, resolvendo o UnknownDependenciesException

--- api/src/modules/buckets/buckets.module.ts
+++ api/src/modules/buckets/buckets.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ApiKeysModule } from '../api-keys/api-keys.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { BucketsApiController } from './buckets-api.controller';
 import { BucketsController } from './buckets.controller';
 import { BucketsService } from './buckets.service';
 
 @Module({
+  imports: [PrismaModule, ApiKeysModule],
   controllers: [BucketsController, BucketsApiController],
   providers: [BucketsService],
   exports: [BucketsService]
